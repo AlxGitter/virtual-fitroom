32afacd16acaa78819907feae1fd6a60fd59f342
AlxGitter <alexander.wittmann0404@gmail.com>
2025-05-02 14:47:31 +0200

    🚀 Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>👕 Virtual Fitroom</h1>
+      <p>Hier entsteht deine virtuelle Ankleide-App!</p>
+    </div>
+  )
+}
+
+export default App
